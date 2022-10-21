dd8be7e2cffdb7feb8c9b2eff6f222dc02fa5a32
Kevin Otero Scaloni <kevinotero1a@gmail.com>
2022-10-21 20:27:49 +0200

    Created Rectangle class

---
+++ Rectangle.cpp
@@ -0,0 +1,20 @@
+#include "Rectangle.h"
+
+#include <iostream>
+
+Rectangle::Rectangle(real32 width, real32 height)
+	: width(width), height(height)
+{
+
+}
+
+real32 Rectangle::calculateArea()
+{
+	real32 area = width * height;
+	return area;
+}
+
+void Rectangle::print()
+{
+	std::cout << "(Rectangle) " << width << " " << height << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-#include "Geometry.h"
+#include "Rectangle.h"
 
 int main()
 {
 	std::cout << "Test" << std::endl;
 	
+	Rectangle rect(100, 200);
+	rect.print();
+	std::cout << rect.calculateArea();
 
 	return 0;
 }
