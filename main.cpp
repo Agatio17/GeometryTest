1e906db3b4410733ec6b31961eafa244ae5769d0
Kevin Otero Scaloni <kevinotero1a@gmail.com>
2022-11-20 19:15:45 +0100

    Added geometry count

--- main.cpp
+++ main.cpp
@@ -10,11 +10,15 @@ int main()
 	std::cout << "Test" << std::endl;
 	
 	std::vector<Geometry*> geometries;
+	int nRects = 0;
+	int nCircles = 0;
 	for (int i = 0; i < 100; i++) {
 		if (i % 2 == 0) {
 			geometries.push_back(new Rectangle((real32)i, (real32)i));
+			nRects++;
 		} else {
 			geometries.push_back(new Circle((real32)i));
+			nCircles++;
 		}
 	}
 
